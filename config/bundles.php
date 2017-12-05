e59c13de608fcf9ca5133edfe60e1f3a7a0a658e
kyub <quentin.burgess@epitech.eu>
2017-12-05 13:49:53 +0100

    web porfiler

--- config/bundles.php
+++ config/bundles.php
@@ -12,4 +12,5 @@ return [
     FOS\UserBundle\FOSUserBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
