d666b4fb5dc05aadb90bf60cb23c0e3a5de26bd7
kyub <quentin.burgess@epitech.eu>
2017-12-06 20:09:05 +0100

    cl

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -6,28 +6,16 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 
-
 class DefaultController extends Controller
 {
     /**
-     * @Route(
-     *     "/{name}",
-     *     requirements={"name" = "\w"},
-     *     defaults={"name" = "caca"}
-     * )
+     * @Route("/")
      *
-     * @param $name
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function indexAction($name)
+    public function indexAction()
     {
-        if ($this->getUser()) {
-            return $this->redirectToRoute('app_home_home');
-        }
-
-        return $this->render('hello.html.twig', [
-            'name' => $name
-        ]);
+        return $this->redirectToRoute('app_home_home');
     }
 
 }
